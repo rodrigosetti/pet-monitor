8a64bfec68e32322055c669ccc25da70efc10576
Rodrigo Setti <rodrigosetti@gmail.com>
2017-04-04 17:52:49 -0700

    Configs to enable/disable serial & server

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ const CONTAINER_TARE = config.get('container_tare');
 const CALIBRATION_G = config.get('calibration_g');
 const STABLE_N = config.get('stable_n');
 
-winston.level = 'debug';
-winston.cli();
+winston.level = config.get('log.level');
+if (config.get('log.cli_mode')) {
+    winston.cli();
+}
 
-var db = new sqlite3.Database('data.db');
+let db = new sqlite3.Database('data.db');
 
-let port = new SerialPort(config.get('serial_port'), {
-    baudRate: config.get('baudrate'),
-    parser: SerialPort.parsers.readline('\n')
-});
+let port;
 
 function atExit(exitCode) {
     winston.info(`finalizing with exit code ${exitCode}`);
@@ -45,110 +44,120 @@ let weightWindow = [];
 var lastStable = undefined;
 var temperature = undefined;
 
-port.on('data', (line) => {
-    line = line.trim();
+if (config.get('enable_serial')) {
 
-    if (line == 'Readings:') {
-        ready = true;
-    } else if (ready) {
-        let parts = line.split(',');
+    port = new SerialPort(config.get('serial_port'), {
+        baudRate: config.get('baudrate'),
+        parser: SerialPort.parsers.readline('\n')
+    });
 
-        let weight = calibratedScale( parseFloat(parts[2]) )
-                     - CONTAINER_TARE;
-        temperature = parseFloat(parts[3]);
-        let now = Date.now() / 1000; // in seconds
+    port.on('data', (line) => {
+        line = line.trim();
 
-        winston.debug('weight: %s g, stable: %s g, temp: %s C',
-                weight, lastStable, temperature);
+        if (line == 'Readings:') {
+            ready = true;
+        } else if (ready) {
+            let parts = line.split(',');
 
-        weightWindow.push( weight );
-        if (weightWindow.length > STABLE_N) { weightWindow.shift(); }
+            let weight = calibratedScale( parseFloat(parts[2]) )
+                         - CONTAINER_TARE;
+            temperature = parseFloat(parts[3]);
+            let now = Date.now() / 1000; // in seconds
 
-        let stable = weightWindow.length >= STABLE_N &&
-                     weightWindow.every((e, i, a) =>
-                             { return e === a[i>0? i-1 : i] });
+            winston.debug('weight: %s g, stable: %s g, temp: %s C',
+                    weight, lastStable, temperature);
 
-        if (stable) {
-            if (lastStable !== undefined) {
-                let delta = weight - lastStable
+            weightWindow.push( weight );
+            if (weightWindow.length > STABLE_N) { weightWindow.shift(); }
 
-                if (Math.abs(delta) > 0) {
-                    if (delta > 0) {
-                        // fill
-                        winston.info('FILL: %s g', delta);
-                    } else if (delta < 0) {
-                        // consume
-                        winston.info('CONSUMED: %s g', -delta);
+            let stable = weightWindow.length >= STABLE_N &&
+                         weightWindow.every((e, i, a) =>
+                                 { return e === a[i>0? i-1 : i] });
+
+            if (stable) {
+                if (lastStable !== undefined) {
+                    let delta = weight - lastStable
+
+                    if (Math.abs(delta) > 0) {
+                        if (delta > 0) {
+                            // fill
+                            winston.info('FILL: %s g', delta);
+                        } else if (delta < 0) {
+                            // consume
+                            winston.info('CONSUMED: %s g', -delta);
+                        }
+                        db.run('INSERT INTO deltas VALUES (NULL, $now, $delta, $weight, $temperature)',
+                                {
+                                    $now: now,
+                                    $delta: delta,
+                                    $weight: weight,
+                                    $temperature: temperature
+                                });
                     }
-                    db.run('INSERT INTO deltas VALUES (NULL, $now, $delta, $weight, $temperature)',
-                            {
-                                $now: now,
-                                $delta: delta,
-                                $weight: weight,
-                                $temperature: temperature
-                            });
                 }
+
+                lastStable = weight;
             }
-
-            lastStable = weight;
-        }
-    }
-});
-
-let app = express();
-
-app.set('view engine', 'pug');
-app.use(morgan('dev'));
-
-/*
- * "q" can have the following formats:
- * N => since N hour ago
- * N-M => between N and M hours ago
- */
-app.get('/', (req, res) => {
-    let query = req.query.q || '24';
-    let parts = query.trim().split('-');
-    let sinceH = parseInt(parts[0]);
-    let toH = parts.length > 1 ? parseInt(parts[1]) : 0;
-    let now = Date.now() / 1000;
-    let sinceTS = now - (sinceH * 3600);
-    let toTS = now - (toH * 3600);
-
-    db.all(
-            'SELECT * FROM deltas WHERE timestamp >= $since AND timestamp <= $to ORDER BY timestamp DESC',
-            {
-                $since : sinceTS,
-                $to : toTS
-            },
-            (err, rows) => {
-
-        if (err) {
-            winston.error(err);
-            res.send('Sorry, an error occurred. Go back and try again');
-        } else {
-            let totalConsumed = rows.reduce((acc, r) => { return acc - (r.delta < 0 ? r.delta : 0); }, 0);
-            let totalFilled = rows.reduce((acc, r) => { return acc + (r.delta > 0 ? r.delta : 0); }, 0);
-
-            // some presentation transformations
-            rows.forEach((r) => {
-                r.timestampStr = dateformat(r.timestamp * 1000, "dayMinute");
-            });
-
-            res.render('index', {
-                query,
-                weightNow: weightWindow.length > 0 ? weightWindow[weightWindow.length-1] : 'unknown',
-                rows,
-                totalConsumed,
-                totalFilled,
-                temperature,
-                sinceStr : dateformat(sinceTS * 1000, "dayMinute"),
-                toStr : dateformat(toTS * 1000, "dayMinute")
-            })
         }
     });
-});
+}
 
-app.listen(8080, () => {
-    winston.info('Server running');
-});
+if (config.get('enable_server')) {
+    let app = express();
+
+    app.set('view engine', 'pug');
+    app.use(morgan(config.get('log.format')));
+
+    /*
+     * "q" can have the following formats:
+     * N => since N hour ago
+     * N-M => between N and M hours ago
+     */
+    app.get('/', (req, res) => {
+        let query = req.query.q || '24';
+        let parts = query.trim().split('-');
+        let sinceH = parseInt(parts[0]);
+        let toH = parts.length > 1 ? parseInt(parts[1]) : 0;
+        let now = Date.now() / 1000;
+        let sinceTS = now - (sinceH * 3600);
+        let toTS = now - (toH * 3600);
+
+        db.all(
+                'SELECT * FROM deltas WHERE timestamp >= $since AND timestamp <= $to ORDER BY timestamp DESC',
+                {
+                    $since : sinceTS,
+                    $to : toTS
+                },
+                (err, rows) => {
+
+            if (err) {
+                winston.error(err);
+                res.send('Sorry, an error occurred. Go back and try again');
+            } else {
+                let totalConsumed = rows.reduce((acc, r) => { return acc - (r.delta < 0 ? r.delta : 0); }, 0);
+                let totalFilled = rows.reduce((acc, r) => { return acc + (r.delta > 0 ? r.delta : 0); }, 0);
+
+                // some presentation transformations
+                rows.forEach((r) => {
+                    r.timestampStr = dateformat(r.timestamp * 1000, "dayMinute");
+                });
+
+                res.render('index', {
+                    query,
+                    weightNow: weightWindow.length > 0 ? weightWindow[weightWindow.length-1] : 'unknown',
+                    rows,
+                    totalConsumed,
+                    totalFilled,
+                    temperature,
+                    sinceStr : dateformat(sinceTS * 1000, "dayMinute"),
+                    toStr : dateformat(toTS * 1000, "dayMinute")
+                })
+            }
+        });
+    });
+
+    app.listen(8080, () => {
+        winston.info('Server running');
+    });
+}
 
